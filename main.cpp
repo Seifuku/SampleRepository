a473f8f45453b9cb2302d8df14c272746e9a30e2
Kyosuke Miura <kyosuke.miura@ams.eng.osaka-u.ac.jp>
2016-07-25 18:03:52 +0900

    main.cppを作った。

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <vector.h>
+
+int main() {
+
+  std::vector<double> array;
+  for(double x = 0.; x < 10.; x += 1.)
+    array.push_back(x);
+
+  printf("size=%d\n", array.size());
+
+  return 0;
+
+}
